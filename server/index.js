79bf7eef573d87cc37b08fdbda1fe7e88825fcc5
Mariano Masondo <nanomason@gmail.com>
2026-05-01 15:14:03 -0300

    Fix backend deply for render

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
 
 conn
   .sync({ force: false })
   .then(() => {
-    server.listen(PORT, () => {
+    server.listen(PORT, "0.0.0.0", () => {
       console.log(`Server listening on port ${PORT}`);
     });
   })
