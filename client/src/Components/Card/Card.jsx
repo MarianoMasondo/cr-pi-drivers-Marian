685f5560afd478865b9efb638f15be5a80bc41ce
Mariano Masondo <nanomason@gmail.com>
2026-05-01 16:42:19 -0300

    Fix driver card

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -4,11 +4,20 @@ import { Link } from "react-router-dom";
 
 const Card = ({ id, name, lastname, teams, image }) => {
   return (
-    <Link to={`/detail/${id}`} style={{ textDecoration: 'none' }}>
+    <Link to={`/detail/${id}`} style={{ textDecoration: "none" }}>
       <div className="card">
         <div>
-          <img src={image} alt={name} className="image" />
+          <img
+            src={image || "/default-driver.jpg"}
+            alt={`${name} ${lastname}`}
+            className="image"
+            onError={(e) => {
+              e.currentTarget.onerror = null;
+              e.currentTarget.src = "/default-driver.jpg";
+            }}
+          />
         </div>
+
         <div className="card-info">
           <span>
             {name} {lastname}
@@ -24,11 +33,11 @@ const Card = ({ id, name, lastname, teams, image }) => {
 };
 
 Card.propTypes = {
-  id: PropTypes.string.isRequired,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   name: PropTypes.string.isRequired,
   lastname: PropTypes.string.isRequired,
-  teams: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  teams: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
+  image: PropTypes.string,
 };
 
 export default Card;
